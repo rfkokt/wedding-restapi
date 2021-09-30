7ab74b6defc40313e7877b72c418319dd94e342f
rfkokt <44221016+rfkokt@users.noreply.github.com>
2021-09-30 23:21:24 +0700

    update url

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,3 @@
 module.exports ={
-    url : "mongodb://localhost:27017/collect_rdev"
-
+    url : `mongodb+srv://Rdev:99585767aA@moexpress.6jwoa.mongodb.net/collect_rdev?retryWrites=true&w=majority`
 }
